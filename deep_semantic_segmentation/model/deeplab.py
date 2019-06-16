b1d10410fb3eaf53fd3cb0238d9817941ba94ca3
asahi417 <spacefunkspacefunk@gmail.com>
2019-06-16 16:04:22 +0900

    build: add pascal tfrecord

--- deep_semantic_segmentation/model/deeplab.py
+++ deep_semantic_segmentation/model/deeplab.py
@@ -187,10 +187,8 @@ class DeepLab:
             def get_summary(__name):
                 logging_image_number = 5
                 vis_label = util_tf.coloring_segmentation(self.__segmentation,
-                                                          self.__iterator.num_class,
                                                           [self.__iterator.crop_height, self.__iterator.crop_width])
                 vis_pred = util_tf.coloring_segmentation(self.__prediction,
-                                                         self.__iterator.num_class,
                                                          [self.__iterator.crop_height, self.__iterator.crop_width])
                 __summary_img = tf.summary.merge([
                     tf.summary.image('%s_image' % __name, tf.cast(self.__image, tf.uint8), logging_image_number),
